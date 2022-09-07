e4a6e23704ea1493ff276a7dbe31ccb2bc15e433
Matta-JJ <107634436+Matta-JJ@users.noreply.github.com>
2022-09-06 22:03:02 -0500

    added game stoppage to when player and snail collide, next step s is making the code more organized all the basics of the game have been implemented

--- gamenotes.py
+++ gamenotes.py
@@ -20,6 +20,8 @@ clock = pygame.time.Clock() #frame rate
 #test_font = pygame.font.Font(font type, font size)
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50) #creating a font (make sure to captialize second font)
 
+game_active = True
+
 sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha() #.convert helps convert the image into something that pygame can work with much easier
 ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
 
@@ -42,6 +44,8 @@ snail_rect = snail_surf.get_rect(bottomright = (600,300))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
+player_gravity = 0
+game_active = True
 
 
 while True:
@@ -51,45 +55,61 @@ while True:
 			pygame.quit()
 			exit() #look NOTE2 for explanation
 
-		if event.type == pygame.MOUSEBUTTONDOWN: #check if any mouse button pressed
-			if player_rect.collidepoint(event.pos):
-				player_gravity = -20
+		if game_active:
+			if event.type == pygame.MOUSEBUTTONDOWN: #check if any mouse button pressed
+				if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+					player_gravity = -20
 
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_SPACE:
-				print('jump')
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+					player_gravity = -20
+
+		else:
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+				game_active = True
+
+	if game_active:
+		#draw all our elements
+		#update everything
+		#screen.blit(surface,position) #bloock mage transfer put one mage on another image
+		screen.blit(sky_surface,(0,0))
+		screen.blit(ground_surface,(0,300))
+		pygame.draw.rect(screen, '#c0e8ec', score_rect)
+		pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+		screen.blit(score_surf, score_rect) #third step to creating a font
+		#snail_x_pos -= 4 #this is so that every time the loop runs we move the snails position by 1 so the snail will be moving about 60 frames every second
+			#left is -= 1 and right is += 1
+		#Our next problem is that we need something to help when the snail moves out of the display window so we use an if statement
+		#if snail_surface 
+		#if snail_x_pos < -100: snail_x_pos = 800
+		snail_rect.x -= 4
+		if snail_rect.right <= 0: snail_rect.left = 800
+		screen.blit(snail_surf,snail_rect)
+
+		#Player
+		player_gravity += 1
+		player_rect.y += player_gravity
+		#player_rect.left += 1
+		if player_rect.bottom >= 300: player_rect.bottom = 300 #everytime the player is exceeding the ground we are going to set the player on top of the ground
+		screen.blit(player_surf,player_rect)
+
+		#keys = pygame.key.get_pressed()
+		#if keys[pygame.K_SPACE]:
+		#	print('jump')
+
+		#if player.rect.colliderect(snail_rect): #this returns either a 0 or 1 if there is no collision we get a one if there is a collision we get a 1
+		#rect1.collidepoint(x,y) is important for when you are using clickers
+
+		#mouse_pos = pygame.mouse.get_pos()
+		#if player_rect.collidepoint((mouse_pos)):
+			#pygame.mouse.get_pressed
 
 
-	#draw all our elements
-	#update everything
-	#screen.blit(surface,position) #bloock mage transfer put one mage on another image
-	screen.blit(sky_surface,(0,0))
-	screen.blit(ground_surface,(0,300))
-	pygame.draw.rect(screen, '#c0e8ec', score_rect)
-	pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
-	screen.blit(score_surf, score_rect) #third step to creating a font
-	#snail_x_pos -= 4 #this is so that every time the loop runs we move the snails position by 1 so the snail will be moving about 60 frames every second
-		#left is -= 1 and right is += 1
-	#Our next problem is that we need something to help when the snail moves out of the display window so we use an if statement
-	#if snail_surface 
-	#if snail_x_pos < -100: snail_x_pos = 800
-	snail_rect.x -= 4
-	if snail_rect.right <= 0: snail_rect.left = 800
-	screen.blit(snail_surf,snail_rect)
-	#player_rect.left += 1
-	if player_rect.bottom >= 300: player_rect.bottom = 300 #everytime the player is exceeding the ground we are going to set the player on top of the ground
-	screen.blit(player_surf,player_rect)
-
-	#keys = pygame.key.get_pressed()
-	#if keys[pygame.K_SPACE]:
-	#	print('jump')
-
-	#if player.rect.colliderect(snail_rect): #this returns either a 0 or 1 if there is no collision we get a one if there is a collision we get a 1
-	#rect1.collidepoint(x,y) is important for when you are using clickers
-
-	#mouse_pos = pygame.mouse.get_pos()
-	#if player_rect.collidepoint((mouse_pos)):
-		#pygame.mouse.get_pressed
+		#collisions
+		if snail_rect.colliderect(player_rect):
+			game_active = False
+	else:
+		screen.fill('Yellow')
 
 	pygame.display.update() #ths will update the display surface that we had created earlier
 	clock.tick(60) #tells pygame that while true loop shouldnt run faster than 6-fps
@@ -115,3 +135,8 @@ while True:
 			#there's two methods because when using classes you want the controls isnide of the relevant class. Pygame.mouse and pygame.keys are great for that
 	# creating the floor, check the collision between player and floor and move player up if collision
 		#all the we really need is to check y position of the player player.bottom > 300: player.bottom = 300
+#NOTE9 game over state when are checkng if the snal rectangle s coliding with the colde rectangle
+	#game and game over - when player colldes with object it will stop the game and show the score. We need to put the entire game in an if statement. This is basic logic and not recommended 
+
+
+
