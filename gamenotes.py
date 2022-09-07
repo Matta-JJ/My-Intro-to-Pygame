d70292fb683862952810d7494fe9f1d4ec9df65d
jarjuf <jarjuf@oregonstate.edu>
2022-09-06 19:41:31 -0500

    added a new file where I have production code and then used my other file for notes

--- gamenotes.py
+++ gamenotes.py
@@ -3,7 +3,6 @@ import pygame #When starting a new pygame project import it and then run to see 
 from sys import exit #look NOTE2 for explanations
 
 pygame.init() #this is to initialize pygame, it's like starting the engine of a car
-
 #The next thing to do is create a display surface, this window the player is going
 	#to see at the end - screen = pygame.display.set_mode((width,height)) 
 screen = pygame.display.set_mode((800,400)) 
@@ -30,10 +29,13 @@ ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
 		#1 Create a font (text size and style) 2. Write text on a surface 3. Blit the text surface (so place the surface on the actual image)
 #text_surface = test_font.render(text_info, Anti Aliasing, color) # second step which is writing a text on a surface #render needs three bits of information
 	# Anti Aliasing means we smooth the edges of the text, which is not necessary in pixel art, but any other text you want it to be true
-text_surface = test_font.render('My Game', False, 'Black')
+score_surf = test_font.render('My Game', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400,50)
+
 
 #Next we are wanting to move the snail from the left side of the image to the right
-snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha() #just this is static and we need it to move
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+#snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha() #just this is static and we need it to move
 #snail_x_pos = 600 # we moved the 600 that was in the snail position into a variable
 snail_rect = snail_surf.get_rect(bottomright = (600,300))
 
@@ -47,8 +49,12 @@ while True:
 		if event.type == pygame.QUIT: #looking to see if the player input(event) was quit because if it was then the player can exit the game this will repeat each frame
 			pygame.quit()
 			exit() #look NOTE2 for explanation
-		if event.type == pygame.MOUSEMOTION:
-			event.pos
+		#if event.type == pygame.MOUSEMOTION:
+			#if player_rect.collidepoint(event.pos): print('collision')
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE:
+				print('jump')
 
 
 	#draw all our elements
@@ -56,7 +62,9 @@ while True:
 	#screen.blit(surface,position) #bloock mage transfer put one mage on another image
 	screen.blit(sky_surface,(0,0))
 	screen.blit(ground_surface,(0,300))
-	screen.blit(text_surface, (300, 50)) #third step to creating a font
+	pygame.draw.rect(screen, '#c0e8ec', score_rect)
+	pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+	screen.blit(score_surf, score_rect) #third step to creating a font
 	#snail_x_pos -= 4 #this is so that every time the loop runs we move the snails position by 1 so the snail will be moving about 60 frames every second
 		#left is -= 1 and right is += 1
 	#Our next problem is that we need something to help when the snail moves out of the display window so we use an if statement
@@ -64,16 +72,20 @@ while True:
 	#if snail_x_pos < -100: snail_x_pos = 800
 	snail_rect.x -= 4
 	if snail_rect.right <= 0: snail_rect.left = 800
-	screen.blit(snail_surf, snail_rect)
+	screen.blit(snail_surf,snail_rect)
 	#player_rect.left += 1
-	screen.blit(player_surf, player_rect)
+	screen.blit(player_surf,player_rect)
 
-	if player.rect.colliderect(snail_rect): #this returns either a 0 or 1 if there is no collision we get a one if there is a collision we get a 1
-#rect1.collidepoint(x,y) is important for when you are using clickers
+	#keys = pygame.key.get_pressed()
+	#if keys[pygame.K_SPACE]:
+	#	print('jump')
 
-	mouse_pos = pygame.mouse.get_pos()
-	if player_rect.collidepoint((mouse_pos)):
-		pygame.mouse.get_pressed
+	#if player.rect.colliderect(snail_rect): #this returns either a 0 or 1 if there is no collision we get a one if there is a collision we get a 1
+	#rect1.collidepoint(x,y) is important for when you are using clickers
+
+	#mouse_pos = pygame.mouse.get_pos()
+	#if player_rect.collidepoint((mouse_pos)):
+		#pygame.mouse.get_pressed
 
 	pygame.display.update() #ths will update the display surface that we had created earlier
 	clock.tick(60) #tells pygame that while true loop shouldnt run faster than 6-fps
@@ -93,3 +105,8 @@ while True:
 #NOTE6: How to animate in pygame. We are not drawing static image, but we are updating the image over and over again 60 times a second and placing it in the same place
 	# animating each just means changing the position slight on each frame
 #NOTE7: Rectangles, Precise positioning of surfaces, helps avoid basic collisions 
+#NOTE8: the player character. Keyboard input, jump + gravity, creating a floor
+	#two ways to get keyboard input is with method or through event loop
+		#Keyboard input in the event loop 1. check if any button was pressed 2 work with a specific key
+			#there's two methods because when using classes you want the controls isnide of the relevant class. Pygame.mouse and pygame.keys are great for that
+	
